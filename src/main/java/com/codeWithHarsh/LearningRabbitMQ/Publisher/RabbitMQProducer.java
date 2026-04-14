2b0f8430c83f34d723dfcf9042667a37b334f575
HarshVathare <harsh1234vathare@gmail.com>
2026-04-14 17:56:19 +0530

    send msg succuss in RabbitMQ

--- src/main/java/com/codeWithHarsh/LearningRabbitMQ/Publisher/RabbitMQProducer.java
+++ src/main/java/com/codeWithHarsh/LearningRabbitMQ/Publisher/RabbitMQProducer.java
@@ -1,5 +1,6 @@
 package com.codeWithHarsh.LearningRabbitMQ.Publisher;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.logging.Logger;
 
 @Service
+@Slf4j
 public class RabbitMQProducer {
 
     @Value("${rabbitmq.exchange.name}")
@@ -16,7 +18,7 @@ public class RabbitMQProducer {
     @Value("${rabbitmq.routing.key}")
     private String routingKey;
 
-    private static final Logger LOGGER = (Logger) LoggerFactory.getLogger(RabbitMQProducer.class);
+//    private static final Logger LOGGER = (Logger) LoggerFactory.getLogger(RabbitMQProducer.class);
 
     private final RabbitTemplate rabbitTemplate;
 
@@ -25,7 +27,7 @@ public class RabbitMQProducer {
     }
 
     public void sendMessage(String message) {
-        LOGGER.info(String.format("Message sent -> %s", message));
+        log.info(String.format("Message sent -> %s", message));
         rabbitTemplate.convertAndSend(exchange, routingKey, message);
 
     }

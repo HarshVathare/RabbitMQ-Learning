4e8f3c5cb59717125714ac6d3056d11d08ee6828
HarshVathare <harsh1234vathare@gmail.com>
2026-04-14 17:08:10 +0530

    create rabbitMQ consept learnig

---
+++ src/main/java/com/codeWithHarsh/LearningRabbitMQ/LearningRabbitMqApplication.java
@@ -0,0 +1,13 @@
+package com.codeWithHarsh.LearningRabbitMQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningRabbitMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningRabbitMqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeWithHarsh/LearningRabbitMQ/LearningRabbitMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeWithHarsh.LearningRabbitMQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningRabbitMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
